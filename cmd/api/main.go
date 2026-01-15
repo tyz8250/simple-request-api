a3995b93b8defed30fd2cc4bc797cb5275cf64fd
tyz8250 <tyz0308@gmail.com>
2026-01-16 07:29:29 +0900

    memory-add

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -5,14 +5,16 @@ import (
 	"net/http"
 
 	"github.com/ricosh/simple-request-api/internal/handler"
+	"github.com/ricosh/simple-request-api/internal/repository"
 	request_service "github.com/ricosh/simple-request-api/internal/service"
 )
 
 func main() {
-	requestService := &request_service.RequestService{}
-	requestHandler := handler.NewRequestHandler(requestService)
+	repo := repository.NewMemoryRequestRepository()
+	service := request_service.NewRequestService(repo)
+	handler := handler.NewRequestHandler(service)
 
-	http.HandleFunc("/requests", requestHandler.GetRequests)
+	http.HandleFunc("/requests", handler.GetRequests)
 
 	log.Println("Server started at :8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
--- internal/repository/request_repository.go
+++ internal/repository/request_repository.go
@@ -2,7 +2,9 @@ package repository
 
 import "github.com/ricosh/simple-request-api/internal/model"
 
-// RequestRepository は、リクエストの保存方法と取得方法を定義します。
+// RequestRepository は、リクエストの保存方法と取得方法を定義。
+// interface なので、実装は別ファイルに定義する。(DB実装に依存しない、テストや差し替えを容易にする)
+
 type RequestRepository interface {
 	FindAll() ([]model.Request, error)
 	FindByID(id int64) (model.Request, error)
